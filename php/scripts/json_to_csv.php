38b030ff2673cd5edb2c32939adf34d7217e3542
John Brahy <john@brahy.com>
2018-03-14 13:00:57 -0700

    added json_to_csv

---
+++ php/scripts/json_to_csv.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jbrahy
+ * Date: 4/6/17
+ * Time: 10:35
+ */
+
+
+if ($argc == 3) {
+
+	$contents = file_get_contents($argv[1]);
+	$fp = fopen($argv[2], 'w');
+
+	$json = json_decode($contents, TRUE);
+
+	$output = array();
+	$header = array_keys($json['records'][0]);
+
+	fputcsv($fp, $header);
+
+	foreach ($json['records'] as $row) {
+
+		fputcsv($fp, $row);
+	}
+
+	fclose($fp);
+
+} else {
+	printf("%s <input file> <output file>\n", $argv[0]);
+}
