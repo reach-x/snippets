5ee9e2729ca366efcdeecdcb2e51fbb04d8718f4
John Brahy <john@brahy.com>
2019-06-04 15:00:23 -0700

    adding a couple new tools

---
+++ php/scripts/ip_information.php
@@ -0,0 +1,43 @@
+<?php
+
+if (isset($argv[1])) {
+	$filename = $argv[1];
+} else {
+	die("Need argument of ips to script");
+}
+
+class database {
+
+	public $username = "root";
+	public $password = "<password>";
+	public $name = "ip_info";
+	public $server = "db01.mypm.us";
+	public $port = 3306;
+}
+
+$database = new database();
+$lines = file($filename);
+$first_row = TRUE;
+
+
+foreach ($lines as $line) {
+
+	$ip_address = trim($line);
+	$ip_address_number = ip2long($ip_address);
+	$sql = sprintf("SELECT * FROM ip_info.ip_address_data WHERE ip_to > '%s' LIMIT 1", $ip_address_number);
+	$mysqli = new mysqli($database->server, $database->username, $database->password, $database->name, $database->port);
+
+	if ($mysqli->connect_errno) {
+		printf("Error:%s %s \n", $mysqli->connect_errno, $mysqli->connect_error);
+	}
+
+	$result = $mysqli->query($sql);
+	$row = $result->fetch_array(MYSQLI_ASSOC);
+
+	if ($first_row) {
+		printf("ip_address,%s\n", join(",", array_keys($row)));
+		$first_row = FALSE;
+	}
+
+	printf("%s,%s\n", $ip_address, join(",", $row));
+}
--- php/tmp/working.php
+++ php/tmp/working.php
@@ -13,3 +13,59 @@ FROM tbl_comment
 ORDER BY tbl_like_unlike.like_unlike DESC, tbl_comment.date DESC
 EOQ;
 
+
+function pull_from_api() {
+
+	$page_limit = 1000;
+
+	$top_limit = $this->get_count() + 1000;
+
+	$campaigns = array();
+	$offset = 0;
+
+	while ($offset < $top_limit) {
+
+		printf("Pulling %d to %d\n", $offset, ($offset + $page_limit));
+
+		$url = "http://api.1318amethyst.com/api/v1.1/campaigns?all_details=1&offset={$offset}&limit={$page_limit}";
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			"auth_token:6d4888962feba3c669176b81edacb185",
+			"accept:application/json",
+		));
+
+		$raw_response = curl_exec($ch);
+
+		if (curl_errno($ch) > 0) {
+			print_r(array(
+				'error' => curl_error($ch),
+				'response' => $raw_response,
+			));
+		}
+		$campaigns = $this->append_campaigns($campaigns, json_decode($raw_response));
+
+		curl_close($ch);
+
+		$offset += $page_limit;
+	}
+
+	printf("Pulled %d campaigns\n", count($campaigns));
+
+	return $campaigns;
+
+}
+
+
+
+function append_campaigns($existing_campaigns, $more_campaigns) {
+
+	foreach ($more_campaigns->campaigns as $campaigns) {
+		$existing_campaigns[] = $campaigns;
+	}
+
+	return $existing_campaigns;
+}
